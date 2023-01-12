f5d038ad02087f0cb4a82061e399adc92e66d599
ForKorny <81922393+ForKorny@users.noreply.github.com>
2023-01-12 17:48:13 +0200

    Update server.py

--- server.py
+++ server.py
@@ -3,8 +3,8 @@ import socket
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('127.0.0.1', 2000))
 
-x = 10000
-y = 10000
+x = 16740
+y = 14850
 flag = 0
 bites = str([flag, x, y]).encode()
 

b78f6ce59c5e29103fd76f48abe91dbea747955a
ForKorny <81922393+ForKorny@users.noreply.github.com>
2023-01-12 17:27:45 +0200

    Add files via upload

---
+++ server.py
@@ -0,0 +1,17 @@
+import socket
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(('127.0.0.1', 2000))
+
+x = 10000
+y = 10000
+flag = 0
+bites = str([flag, x, y]).encode()
+
+server.listen(30)
+client_socket, adress = server.accept()
+
+
+data = client_socket.recv(1024)
+
+client_socket.send(bites)
